c9b5e093f3cb68a6f7208ababb2a321137639ddd
amoslgl96 <baolgl96@gmail.com>
2020-03-28 21:16:12 +0800

    new major version v2.0.0

--- calc/math.go
+++ calc/math.go
@@ -1,11 +1,16 @@
 package calc
 
+import "errors"
+
 //Add returns sum of the ints stored in variadic arg numbers
-func Add(numbers ...int) int {
+func Add(numbers ...int) (error,int) {
 	sum := 0
+	if len(numbers) < 2 {
+		return errors.New("Provided less than 2 numbers"),sum
+	}
 	for _,n := range numbers {
 		sum += n
 	}
 
-	return sum
+	return nil,sum
 }
